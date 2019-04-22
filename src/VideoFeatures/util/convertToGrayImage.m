39e6821b452daf8a854c9be276c6d24f7b3e66fe
ki.jasonj@gmail.com <jobenmoe@gmail.com>
2019-04-22 19:39:39 -0400

    deleted submodules

--- src/VideoFeatures/util/convertToGrayImage.m
+++
@@ -1,5 +0,0 @@
-function image = convertToGrayImage(image)
-if numel(size(image)) > 2
-    image = rgb2gray(image);
-end
-end
--- src/VideoFeatures/util/resizeVideo.m
+++
@@ -1,9 +0,0 @@
-function videoOut = resizeVideo(video, ratio)
-
-[numFrames, height, width, numChannels] = size(video);
-
-for i = 1:numFrames
-    frame = squeeze(video(i,:,:,:));
-    frameResized = imresize(frame, ratio);
-    videoOut(i,:,:,:) = frameResized;
-end
